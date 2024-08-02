6408015153930c6c03080accabcda1a54af56c7c
Cesar B Rocha <rocha.cesarb@protonmail.com>
2024-08-02 09:23:43 -0300

    rebuilding site Fri Aug  2 09:23:37 -03 2024

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+# script from on the m2lines site
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mRebuilding website and pushing changes to GitHub...\033[0m\n"
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+        msg="$*"
+fi
+
+# first pull
+git pull && git add --all && git commit -m "$msg" && git push --set-upstream origin master;
+
+# Build the project.
+hugo -t hugo-hero-theme # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
